ce71258435bec0257e170f02526332d48afe2948
Ozan Sazak <ozan.sazak@protonmail.ch>
2022-01-04 19:16:53 +0300

    updated h.c

---
+++ Q4/h.c
@@ -0,0 +1,18 @@
+uf_info_t *fip = P_FINFO(p);
+
+for (int fd = 0; fd < fip->fi_nfiles; fd++) {
+    uf_entry_t *ufp = fip->f_list[fd];
+    file_t *fp = ufp->uf_file;
+
+    if (NULL == fp)
+        continue;
+
+    fp->f_vnode->v_op->vop_close(fp->f_vnode, fp->f_flag, fp->f_count, fp->f_offset, fp->f_cred, NULL);
+
+    if (fp->f_vnode->v_count == 1)
+        fp->f_vnode->v_op->vop_inactive(fp->f_vnode, fp->f_cred, NULL);
+
+    ufp->uf_file = NULL;
+    ufp->uf_flag = 0;
+    fp->f_count--;
+}
