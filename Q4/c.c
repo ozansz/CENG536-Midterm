014b74b7abf4d08c6f2a6408c099890a9028f1de
Ozan Sazak <ozan.sazak@protonmail.ch>
2022-01-04 16:24:12 +0300

    updated c.c

--- Q4/c.c
+++ Q4/c.c
@@ -1,5 +1,9 @@
 void ptree(struct task_struct *p) {
+    if (NULL == p)
+        return;
+
     struct task_struct *c;
+
     list_for_each_entry(c, &p->children, sibling) {
         ptree(c);
         do_something(c);
